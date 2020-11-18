73d10c5cc41a2ec8bfb4d32247b99772f7790155
Simone Messaggi <simone.messaggi@mia-platform.eu>
2020-11-18 13:08:13 +0100

    hello-world tutorial

---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return <div style={{ color: 'purple', fontSize: '1em'}}>
+    <h1>Hello world of fucking Gatsby!</h1>
+    <p>fuck the rest</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+}
